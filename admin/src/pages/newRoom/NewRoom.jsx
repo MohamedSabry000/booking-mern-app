d89a33ad29dcf32e986bd2aa9f869414be954194
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-05-20 00:42:41 +0200

    New User Handling

--- admin/src/pages/newRoom/NewRoom.jsx
+++ admin/src/pages/newRoom/NewRoom.jsx
@@ -1,42 +1,11 @@
-import "./new.scss";
+import "./newRoom.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
-const New = ({ inputs, title }) => {
+const NewRoom = ({ inputs, title }) => {
   const [file, setFile] = useState("");
-const [info, setInfo] = useState({});
-const navigate = useNavigate();
-const handleChange = (e) => {
-  setInfo(prev=>({
-    ...prev,
-    [e.target.id]: e.target.value
-  }));
-}
-
-const handleClick = async (e) => {
-  e.preventDefault();
-  const data = new FormData()
-  data.append("file", file);
-  data.append("upload_preset", "upload"); 
-  try{
-    const uploadRes = await axios.post("https://api.cloudinary.com/v1_1/diohxymdc/image/upload", data);
-    const {url} = uploadRes.data;
-
-    const newUser = {
-      ...info,
-      img: url,
-    }
-
-    await axios.post("/auth/register", newUser);
-    navigate("/users");
-  }catch(err){
-    console.log(err);
-  }
-}
 
   return (
     <div className="new">
@@ -74,15 +43,10 @@ const handleClick = async (e) => {
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input 
-                    onChange={handleChange} 
-                    type={input.type} 
-                    placeholder={input.placeholder} 
-                    id={input.id}
-                  />
+                  <input type={input.type} placeholder={input.placeholder} />
                 </div>
               ))}
-              <button onClick={handleClick}>Send</button>
+              <button>Send</button>
             </form>
           </div>
         </div>
@@ -91,4 +55,4 @@ const handleClick = async (e) => {
   );
 };
 
-export default New;
+export default NewRoom;
